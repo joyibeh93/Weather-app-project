9f569578ad8cd9785e4f2b89e208573b06c968b2
joyibeh93 <joychinwenduibeh93@gmail.com>
2022-10-04 17:15:08 +0100

    added daily forcast

--- src/app.js
+++ src/app.js
@@ -60,16 +60,14 @@ function displayTemperature(response) {
 function getForecast(coordinates) {
     // console.log(coordinates);
     let apiKey = 'e7ac5db1afc40d248972898a4bbd11e2';
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}`;
-    console.log(apiUrl);
-    // let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    // let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    // axios.get(apiUrl).then(displayForecast);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForcast);
 }
 
 // FUNCTION TO  CITY FORCAST
 
-function forcast() {
+function displayForcast(response) {
+    console.log(response.data.daily);
     let forcastElement = document.querySelector('#forecast-daily-weather');
     let forecastHtml = `<div class="row">`;
     let days = ['thur', 'fri', 'sat', 'sun', 'mon', 'tue'];
@@ -139,4 +137,3 @@ celciusLink.onclick = function() {
 };
 
 search('Nigeria');
-forcast();
